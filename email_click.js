f4d779a56ae970a2c9b9712be34ea289d051d9ba
Ninja <muhamadusulaiman41912@gmail.com>
2024-01-23 02:31:27 -0800

    update

---
+++ email_click.js
@@ -0,0 +1,4 @@
+document.getElementById("email-link").addEventListener("click", function (event) {
+  event.preventDefault(); // Prevent the default link behavior
+  window.location.href = "mailto:muhamadusulaiman41912@gmail.com";
+});
