4cb6a077cb5f3235bca8b0b94761d5cc6905f576
Ninja <muhamadusulaiman41912@gmail.com>
2024-01-29 09:44:51 -0800

    update

--- preload.js
+++ preload.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Hide preloader and show content when the page is fully loaded
-    var preloader = document.getElementById("hypno");
+    var preloader = document.getElementById("loader");
 
     window.addEventListener("load", function () {
         preloader.style.display = "none";
---
+++ preload1.js
@@ -0,0 +1,7 @@
+
+window.onload = function () {
+    setTimeout(function () {
+        document.querySelector('#loader').Style.display = 'none';
+        document.querySelector('.hero').Style.display = 'block';
+    }, 2000);
+};
