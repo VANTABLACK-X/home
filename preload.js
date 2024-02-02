c7cec0f2b9c30ee824a5b8dcf9ee76124d5baf46
Ninja <muhamadusulaiman41912@gmail.com>
2024-02-02 07:05:38 -0800

    update

--- preload.js
+++ preload.js
@@ -1,14 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Hide preloader and show content when the page is fully loaded
-    var preloader = document.getElementById("loader");
-    var hr = document.getElementById("hero");
-    var sr = document.getElementById("screen");
+    const preloader = document.getElementById('loader');
+    const navi = document.getElementById('nav');
+    const sr = document.getElementById('screen');
+    const cm22 = document.getElementById('center-main22');
+    const cm23 = document.getElementById('center-main23');
+    const cm32 = document.getElementById('center-main32');
+    const cm33 = document.getElementById('center-main33');
+    const rdp = document.getElementById('r-dp-container');
+    const mem1 = document.getElementById('member1');
+    const mem2 = document.getElementById('member2');
+    const mem3 = document.getElementById('member3');
+    const mem4 = document.getElementById('member4');
 
     window.addEventListener('load', function () {
         preloader.style.display = "none";
-        hr.style.display = "block";
-        sr.style.display = "block";
-    }, 100);
+        navi.style.display = "block";
+        sr.classList.add('sranimation');
+        cm22.classList.add('cm22');
+        cm23.classList.add('cm23');
+        cm32.classList.add('cm32');
+        cm33.classList.add('cm33');
+        rdp.classList.add('rdp');
+        mem1.classList.add('mem1');
+        mem2.classList.add('mem2');
+        mem3.classList.add('mem3');
+        mem4.classList.add('mem4');
+    });
 });
 
 
