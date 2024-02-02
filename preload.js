4c6aad44b8c8567e4e76aae39f8928978c7a57e2
Ninja <muhamadusulaiman41912@gmail.com>
2024-02-02 00:40:59 -0800

    update

--- preload.js
+++ preload.js
@@ -1,8 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Hide preloader and show content when the page is fully loaded
     var preloader = document.getElementById("loader");
+    var hr = document.getElementById("hero");
+    var sr = document.getElementById("screen");
 
-    window.addEventListener("load", function () {
+    window.addEventListener('load', function () {
         preloader.style.display = "none";
-    });
+        hr.style.display = "block";
+        sr.style.display = "block";
+    }, 100);
 });
+
+
+
+
--- preload1.js
+++ preload1.js
@@ -5,3 +5,19 @@ window.onload = function () {
         document.querySelector('.hero').Style.display = 'block';
     }, 2000);
 };
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Hide preloader and show content when the page is fully loaded
+    var hr = document.getElementById("hero");
+    var sr = document.querySelectorAll(".animate")
+
+hr.style.display="block";
+
+    sr.forEach(function(element, index){
+        setTimeout(function() {
+            element.classList.add("animated");
+        }, index * 300);
+    });
+        
+});
