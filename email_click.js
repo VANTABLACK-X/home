e2500636370de78fec7c61c4d6048da358b96d15
Ninja <muhamadusulaiman41912@gmail.com>
2024-02-22 05:50:31 -0800

    update

--- email_click.js
+++ email_click.js
@@ -1,4 +1,4 @@
-document.getElementById("email-link").addEventListener("click", function (event) {
+document.getElementById("mail").addEventListener("click", function (event) {
   event.preventDefault(); // Prevent the default link behavior
-  window.location.href = "mailto:muhamadusulaiman41912@gmail.com";
+  window.location.href = "mailto:vantablack127@gmail.com";
 });
