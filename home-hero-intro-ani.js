7900e3090076aa3189c735d272f8224019428b2c
Ninja <muhamadusulaiman41912@gmail.com>
2024-03-19 03:20:46 -0700

    update

--- home-hero-intro-ani.js
+++ home-hero-intro-ani.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     window.addEventListener('load', function () {
         navi.classList.add('navani');
-        sr.classList.add('sranimation1');
+        sr.classList.add('sranimation');
         cm22.classList.add('cm22');
         cm23.classList.add('cm23');
         cm32.classList.add('cm32');
