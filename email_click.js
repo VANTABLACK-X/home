98a1b9bebe606b2ff5c3cb8c4245c562ea8b3abb
VANTABLACK.ORG.IN <83003977+VANTABLACK-X@users.noreply.github.com>
2024-08-05 12:46:47 +0530

    update

--- email_click.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("mail").addEventListener("click", function (event) {
-  event.preventDefault(); // Prevent the default link behavior
-  window.location.href = "mailto:vantablack127@gmail.com";
-});
